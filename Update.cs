852887e33bd77678a7f9b0d8dbe6ee6687fc157d
bdarshan123 <79901125+bdarshan123@users.noreply.github.com>
2022-05-09 20:59:50 +0530

    Add files via upload

---
+++ Update.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+
+namespace Fundraise.Requests.Campaign
+{
+    public class Update : IRequest<bool>
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DefaultCurrencyCode { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public string MoreInfoUrl { get; set; }
+    }
+}
---
+++ UpdateHandler.cs
@@ -0,0 +1,33 @@
+﻿using Fundraise.Core.Services;
+using Fundraise.Requests.Campaign;
+using MediatR;
+
+namespace Fundraise.RequestHandlers.InProcess.Campaign
+{
+    public class UpdateHandler : RequestHandler<Update, bool>
+    {
+        private ICampaignRepository _campaignRepository;
+
+        public UpdateHandler(CampaignRepository campaignRepository)
+        {
+            _campaignRepository = campaignRepository;
+        }
+
+        protected override bool HandleCore(Update request)
+        {
+            try
+            {
+                var campaign = _campaignRepository.FindById(request.Id);
+                campaign.Name = request.Name;
+                campaign.Description = request.Description;
+                campaign.DefaultCurrencyCode = request.DefaultCurrencyCode;
+                _campaignRepository.Update(campaign);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
