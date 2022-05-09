a39b3157a4e21ce38fe8a66b724088c554ca71a4
bdarshan123 <79901125+bdarshan123@users.noreply.github.com>
2022-05-09 20:51:26 +0530

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using Fundraise.Core.Entities;
+using Fundraise.Core.Services;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using SimpleInjector;
+using Microsoft.EntityFrameworkCore;
+using SimpleInjector.Diagnostics;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+using System.Reflection;
+
+namespace Fundraise.MvcExample
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+
+
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fundraise.Core.Services;
+
+namespace Fundraise.MvcExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ICampaignRepository _campaignRepository;
+        private FundraiserRepository _fundraiserRepository;
+
+        public HomeController(CampaignRepository campaignRepository, FundraiserRepository fundraiserRepository)
+        {
+            _campaignRepository = campaignRepository;
+            _fundraiserRepository = fundraiserRepository;
+        }
+
+        public ActionResult Index()
+        {
+            //var campaign = _campaignRepository.Create("Nicaragua", "USD");
+            //_fundraiserRepository.Create("Alex's fundraiser for school supplies", campaign.Id, Core.Entities.FundraiserType.Individual);
+            //_fundraiserRepository.Create("Deanna's fundraiser for school supplies", campaign.Id, Core.Entities.FundraiserType.Individual);
+
+
+            //var x = _campaignRepository.FindByName("test");
+            //var z = _fundraiserRepository.GetAll();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
